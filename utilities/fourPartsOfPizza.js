3be219a4530ec69efa52d2bdf24ee792d6d8d395
Holub Pavel <golub.pavel2k17@yandex.ru>
2025-05-11 13:54:46 +0300

    create final window

--- utilities/fourPartsOfPizza.js
+++ utilities/fourPartsOfPizza.js
@@ -1,3 +1,4 @@
+// ---Логика отображения пиццы---
 let countBasicsSauce = 2;
 let countMeatIngredients = 0.6;
 let countNonMeatIngredients = 0.6;
@@ -13,24 +14,27 @@ switch (countMain) {
     case 1:
     test1.classList.add('activePizza'); 
     break;
-        case 2:
-        test1.classList.add('activePizza'); 
-        test2.classList.add('activePizza'); 
-        break;
-            case 3:
-            test1.classList.add('activePizza'); 
-            test2.classList.add('activePizza'); 
-            test3.classList.add('activePizza'); 
-            break;
-                case 4:
-                test1.classList.add('activePizza'); 
-                test2.classList.add('activePizza'); 
-                test3.classList.add('activePizza'); 
-                test4.classList.add('activePizza');
-                pizzaBuyClick()
-                break;
-}
 
+    case 2:
+    test1.classList.add('activePizza'); 
+    test2.classList.add('activePizza'); 
+    break;
+
+    case 3:
+    test1.classList.add('activePizza'); 
+    test2.classList.add('activePizza'); 
+    test3.classList.add('activePizza'); 
+    break;
+
+    case 4:
+    test1.classList.add('activePizza'); 
+    test2.classList.add('activePizza'); 
+    test3.classList.add('activePizza'); 
+    test4.classList.add('activePizza');
+    pizzaBuyClick()
+    break;
+}
+// ---Установка обработчика при отображении 4 кусков---
 function pizzaBuyClick () {
     const buyButton = document.querySelector('.buyBlock');
     buyButton.style.color = 'black'
@@ -41,81 +45,126 @@ function pizzaBuyClick () {
         }, 100);        
     })
 }
-
-const sumPrice = 0;
-
-const totalPrice = document.querySelector('.totalPrice')
-totalPrice.textContent = sumPrice
-
+// ---Константы продуктов и их цен---
 const product = [
     {
-    neaplesDough: 3,
-    romeDought: 4,
-    sizilianDough: 6,
-    classicDough: 7
+        neaplesDough: 3,
+        romeDought: 4,
+        sizilianDough: 6,
+        classicDough: 7
     }, {
-    salami: 7,
-    chicken: 5,
-    bacon: 4,
-    pork: 3
+        salami: 7,
+        chicken: 5,
+        bacon: 4,
+        pork: 3
     },{
-    tomato: 2,
-    cheese : 3,
-    truffle: 8,
-    mushrooms: 4
+        tomato: 2,
+        cheese : 3,
+        truffle: 8,
+        mushrooms: 4
     },{
-    id: 'mayou',
-    price: 1,
-    id: 'tomato',
-    price: 2,
-    chesseSauce: 3,
-    bbqSauce: 5
-}];
-
-
+        mayoSauce: 1,
+        tomatoSauce: 2,
+        chesseSauce: 3,
+        bbqSauce: 5
+    }];
+// ---Итоговый набор продуктов---
 const pizzaComposition = {
     dough: null,
-    meat1: null,
-    meat2: null,
-    toppings1: null,
-    toppings2: null,
+    meat: [],
+    toppings: [],
     sauce: null,
 };
-
+// ---Внесение продуктов в итоговый список---
 function selectProduct(category, productName) {
-    const categories = ['dough', 'meat1', 'meat2','toppings1','toppings2', 'sauce'];
+    const categories = ['dough', 'meat', 'toppings', 'sauce'];
     const categoryIndex = categories.indexOf(category);
     
-    // Проверка на несуществующую категорию
-    if (categoryIndex === -1) {
-        console.error(`Категория "${category}" не найдена!`);
-        return false;
-    }
-
     const price = product[categoryIndex][productName];
     
-    // Проверка на несуществующий продукт
-    if (price === undefined) {
-        console.error(`Продукт "${productName}" не найден в категории "${category}"!`);
-        return false;
-    }
+    switch (category) {
+        case 'dough':
+        case 'sauce':
+            pizzaComposition[category] = { name: productName, price: price };
+        break;
 
-    // Сохраняем продукт и его цену
-    pizzaComposition[category] = {
-        name: productName,
-        price: price
-    };
+        case 'meat':
+        case 'toppings':
+            const selectedItems = pizzaComposition[category];
+            if (selectedItems.length >= 2) {
+                console.error(`Можно выбрать не более 2-х вариантов в категории "${category}"!`);
+                return false;
+            }
+            selectedItems.push({ name: productName, price: price });
+            break;
 
-    return true;
+        default:
+            return false;
+        }
+
+return true;
 }
 
-selectProduct('dough', 'classicDough');
-selectProduct('meat1', 'bacon');
-selectProduct('meat2', 'pork');
-selectProduct('toppings1', 'truffle');
-selectProduct('sauce', 'bbqSauce');
+// selectProduct('dough', 'classicDough');
+// selectProduct('meat', 'bacon');
+// selectProduct('meat', 'pork');
+// selectProduct('toppings', 'truffle');
+// selectProduct('toppings', 'tomato');
+// selectProduct('toppings', 'cheese');
+// selectProduct('sauce', 'bbqSauce');
 
-console.log(pizzaComposition);
+// console.log(pizzaComposition);
+
+// ---Логика отображения цены---
+function calcTotalPrice () {
+    let sumPrice = 0;
+    
+    if(pizzaComposition.dough) {
+        sumPrice += pizzaComposition.dough.price
+    }
+    
+    if(pizzaComposition.sauce) {
+        sumPrice += pizzaComposition.sauce.price
+    }
+    
+    pizzaComposition.meat.forEach((elem) => {
+        sumPrice += elem.price
+    })
+    
+    pizzaComposition.toppings.forEach((elem) => {
+        sumPrice += elem.price
+    })
+    
+    return sumPrice
+}
+
+// ---Создание сообщения о составе пиццы и её цены---
+const finalWindow = document.createElement('div');
+finalWindow.classList.add('endWindow');
+const finalWindowWrapper = document.querySelector('.endMessageWrapper').append(finalWindow);
+
+const finalMessage = document.createElement('span');
+finalMessage.textContent = 'Заказ успешно оформлен';
+finalWindow.append(finalMessage);
+
+createCloseButton();
+
+// ---Кнопка закрытия сообщения---
+function createCloseButton() {
+    const closeBtn = document.createElement('button');
+    closeBtn.innerHTML = '&times;'; 
+    closeBtn.classList.add('close-btn'); 
+    closeBtn.addEventListener('click', closeWindow);
+    finalWindow.prepend(closeBtn);
+}
+
+function closeWindow() {
+    document.querySelector('.endWindow').remove('.endWindow');
+    finalWindowWrapper.style.backgroundColor = 'none'; 
+}
+
+
+
 
 
 
